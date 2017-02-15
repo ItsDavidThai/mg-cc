d226d19e5fd3a7fbaaa3acb5c16f17e0e474a903
David Thai <dt@davids-mbp.attlocal.net>
2017-02-14 16:43:51 -0800

    add component layout

---
+++ src/app/feed-container/feed-toolbar/feed-toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed-toolbar',
+  templateUrl: './feed-toolbar.component.html',
+  styleUrls: ['./feed-toolbar.component.css']
+})
+export class FeedToolbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
